7bf03514466287aa7104973bf52f4cab2b681257
Hiromi Giovana Tanita <giovana.hiromin@gmail.com>
2017-02-07 16:58:28 -0200

    Create 2484 - Abracadabra.c

---
+++ 2484 - Abracadabra.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main ( ) {
+    char str[999];
+    int i, j, k;
+
+    while ( scanf ( "%s", str ) != EOF ) {
+        int len = strlen ( str );
+
+        for ( i=0; i<len; i++ ) {
+            k = 0;
+            printf("%.*s", i, "                                                                                                                                  " );
+            for ( j = len - 1 - i; j >= 0; j-- ) {
+                printf ( "%c", str[k++] );
+                if ( j > 0 ) printf ( " " );
+            }
+            puts ( "" );
+        }
+        puts ( "" );
+    }
+}
